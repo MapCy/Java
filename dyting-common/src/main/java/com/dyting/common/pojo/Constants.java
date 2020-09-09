720eaf134228c7a674a300cca66ede23aa3e9cd0
Lambert <2359133163@qq.com>
2020-09-10 07:03:24 +0800

    add Constants Variables

---
+++ dyting-common/src/main/java/com/dyting/common/pojo/Constants.java
@@ -0,0 +1,18 @@
+package com.dyting.common.pojo;
+
+/*
+ * 本项目服务层所需的全局常量
+ */
+public interface Constants {
+
+	//生成文件的路径常量
+	//String FILEPATH = "D:\\java\\workSpace\\dyting-manager\\dyting-manager-web\\src\\main\\webapp\\WEB-INF\\download\\";
+	String FILEPATH = "D:\\apache-tomcat-8.0.49\\webapps\\dyting\\WEB-INF\\download\\";
+	
+	//统计项常量
+	String[] COLUMN = {"country","port","service"};
+	
+	String ERROR = "error";
+	
+	String SUCCESS = "success";
+}
